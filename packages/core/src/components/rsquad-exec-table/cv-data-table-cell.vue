7072833d09a9c5f8ce546d6ea142803d9fce2741
brooney <brooney@rocketsoftware.com>
2019-08-09 11:46:36 -0400

    added data table for executions

---
+++ packages/core/src/components/rsquad-exec-table/cv-data-table-cell.vue
@@ -0,0 +1,23 @@
+<template>
+  <td :style="cellStyle">
+    <cv-tag
+      v-if="filts.includes(this.$slots.default[0].text)"
+      :id="this.$slots.default[0].text + '-active'"
+      kind="cool-gray"
+      :value="this.$slots.default[0].text"
+      :label="this.$slots.default[0].text"
+    ></cv-tag>
+    <slot v-else/>
+
+  </td>
+</template>
+
+<script>
+export default {
+  name: 'CvDataTableCell',
+  data() {return {filts: ["passed", "xpassed", "failed", "xfailed", "error", "skipped"]}},
+  props: {
+    cellStyle: {}
+  },
+};
+</script>
---
+++ packages/core/src/components/rsquad-exec-table/index.js
@@ -0,0 +1,7 @@
+import CvDataTableAction from './cv-data-table-action';
+import CvDataTableCell from './cv-data-table-cell';
+import CvDataTableRow from './cv-data-table-row';
+import RsquadExecTable from './cv-data-table';
+import CvDataTableSkeleton from './cv-data-table-skeleton';
+
+export { RsquadExecTable, CvDataTableRow, CvDataTableCell, CvDataTableAction, CvDataTableSkeleton };
